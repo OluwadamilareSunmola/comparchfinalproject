c382968183c37258462b792e000c28c91e652a98
OSunmol <144754597+OluwadamilareSunmola@users.noreply.github.com>
2024-11-18 01:30:38 -0600

    Add files via upload

---
+++ finalprojmipsfile.asm
@@ -0,0 +1,42 @@
+.data
+
+board1: .space 44
+status1: .space 44
+
+
+problems:
+    prob1:      .asciiz "4x3  "
+    prob2:      .asciiz "5x2  "
+    prob3:      .asciiz "3x6  "
+    prob4:      .asciiz "4x4  "
+    prob5:      .asciiz "2x8  "
+    prob6:      .asciiz "7x2  "
+    prob7:      .asciiz "3x3  "
+    prob8:      .asciiz "6x2  "
+    prob9:      .asciiz "9    "
+    prob10:     .asciiz "4    "
+    prob11:     .asciiz "8x1  "
+    prob12:     .asciiz "2x5  "
+    prob13:     .asciiz "16   "
+    prob14:     .asciiz "7    "
+    prob15:     .asciiz "3x4  "
+    prob16:     .asciiz "5x3  "
+    
+    
+answers:
+    answer1:    .asciiz "12   "
+    answer2:    .asciiz "10   "
+    answer3:    .asciiz "18   "
+    answer4:    .asciiz "16   "
+    answer5:    .asciiz "16   "
+    answer6:    .asciiz "14   "
+    answer7:    .asciiz "9    "
+    answer8:    .asciiz "12   "
+    answer9:    .asciiz "9    "
+    answer10:   .asciiz "4    "
+    answer11:   .asciiz "8    "
+    answer12:   .asciiz "10   "
+    answer13:   .asciiz "16   "
+    answer14:   .asciiz "7    "
+    answer15:   .asciiz "12   "
+    answer16:   .asciiz "15   "
